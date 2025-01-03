a11bf6d80310eaba4d1cfd3a5120334f8dcd1ea3
PranayRawat1200 <pranayrawat51@gmail.com>
2025-01-03 11:40:04 +0530

    init

---
+++ script.js
@@ -0,0 +1,57 @@
+function hianime(){
+    var url='https://en.wikipedia.org/wiki/One_Piece'
+    window.open(url,'_blank').focus
+}
+function my(){
+    var url='https://en.wikipedia.org/wiki/My_Hero_Academia'
+    window.open(url,'_blank').focus
+}
+function dragon(){
+    var url='https://en.wikipedia.org/wiki/Dragon_Ball_Z'
+    window.open(url,'_blank').focus
+}
+function one(){
+    var url='https://en.wikipedia.org/wiki/One-Punch_Man'
+    window.open(url,'_blank').focus
+}
+function haikyu(){
+    var url='https://en.wikipedia.org/wiki/Haikyu!!'
+    window.open(url,'_blank').focus
+}
+function bleach(){
+    var url='https://en.wikipedia.org/wiki/Bleach_(TV_series)'
+    window.open(url,'_blank').focus
+}
+function naruto(){
+    var url='https://en.wikipedia.org/wiki/Naruto'
+    window.open(url,'_blank').focus
+}
+function tokyo(){
+    var url='https://en.wikipedia.org/wiki/Tokyo_Revengers'
+    window.open(url,'_blank').focus
+}
+function hianime(){
+    var url='https://hianime.to/'
+    window.open(url,'_blank').focus
+}function animesuge(){
+    var url='https://animesugetv.to/'
+    window.open(url,'_blank').focus
+}function anime(){
+    var url='https://www.anime-planet.com/'
+    window.open(url,'_blank').focus
+}function crunchyroll(){
+    var url='https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjgy8DijuOJAxWso2YCHdFvDKQYABAAGgJzbQ&ae=2&aspm=1&co=1&ase=2&gclid=CjwKCAiAxea5BhBeEiwAh4t5KxuT40AuIp7MW7K3cQvRgx1sJ627mHHJZefrOsg3tZd0su2WueKLnRoCm7kQAvD_BwE&ohost=www.google.com&cid=CAESVeD2i0rzGZgb-12Nlv3WsDUMb6z573lMJMa_u5fwqw3PY5o3nkHA3E-Pom7SeAS9gisAfYqPWz-ppXDxrc5YUf5aTJtkffy5eqgDM9za2Nw4Rv71j34&sig=AOD64_2Xgeqo4oXJi1pgclFp8rEzndFQ-w&q&nis=4&adurl&ved=2ahUKEwibhrjijuOJAxVOU2wGHYW7FVMQ0Qx6BAgKEAE'
+    window.open(url,'_blank').focus
+}function aniwave(){
+    var url='https://aniwave.uk/'
+    window.open(url,'_blank').focus
+}function a4(){
+    var url='https://4anime.gg/'
+    window.open(url,'_blank').focus
+}function zoro(){
+    var url='https://zorotv.com.in/'
+    window.open(url,'_blank').focus
+}function kaido(){
+    var url='https://kaido.to/'
+    window.open(url,'_blank').focus
+}
